eb15045b25122853e42cb0c8a12664a6287a5f93
Caroline Martins Alves <carol.petrykowski@hotmail.com>
2022-11-06 16:24:13 -0300

    [ISSUE-1] atualizado site com informações do ano de 2022

--- website/src/components/StartPage.vue
+++ website/src/components/StartPage.vue
@@ -3,13 +3,13 @@
     <v-col class="d-flex align-center flex-column" style="min-height: 90vh">
       <v-row class="d-flex align-center mobile-flex-column">
         <div class="text-h1 roboto-mono" v-text="'SECCOM'"></div>
-        <div class="text-h1 blue-text-must-click roboto-mono" v-text="2021"></div>
+        <div class="text-h1 blue-text-must-click roboto-mono" v-text="2022"></div>
       </v-row>
       <v-col class="d-flex flex-column justify-center">
         <v-row class="flex-grow-0 text-h5 roboto-mono">Semana Acadêmica da Computação e Sistemas</v-row>
-        <v-row class="flex-grow-0 text-h5 roboto-mono">De 6 a 10 de dezembro</v-row>
+        <v-row class="flex-grow-0 text-h5 roboto-mono">De XX a XX de dezembro</v-row>
 
-        <v-row class="flex-grow-0 roboto-mono my-8">A SECCOM é um evento acadêmico que visa incrementar a experiência universitária das graduações em Ciência da Computação e Sistemas de Informação, trazendo palestras e oficinas de conteúdos além dos vistos durante a graduação. Esta edição será online e ocorrerá nos dias 6 a 10 de dezembro, com seus eventos ocorrendo majoritariamente no período da tarde. Você pode assistir as transmissões ao vivo no <a href="https://www.youtube.com/channel/UCva2-CmrvppJlf8jSAK61vw">canal no Youtube da SECCOM</a></v-row>
+        <v-row class="flex-grow-0 roboto-mono my-8">A SECCOM é um evento acadêmico que visa incrementar a experiência universitária das graduações em Ciência da Computação e Sistemas de Informação, trazendo palestras e oficinas de conteúdos além dos vistos durante a graduação. Esta edição será online e ocorrerá nos dias XX a XX de dezembro, com seus eventos ocorrendo majoritariamente no período da tarde. Você pode assistir as transmissões ao vivo no <a href="https://www.youtube.com/channel/UCva2-CmrvppJlf8jSAK61vw">canal no Youtube da SECCOM</a></v-row>
       </v-col>
     </v-col>
   </v-container>
